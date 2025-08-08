07f49431841c4a73d15a694226d249ea909ff0d8
selmayh <selmayhn@gmail.com>
2025-08-08 10:39:21 +0200

    Updated running_script.sh : simplified loop by removing array

--- TPSHELL/running_script.sh
+++ TPSHELL/running_script.sh
@@ -6,16 +6,18 @@ repos[devops]="linux git aws"
 repos[data]="python scala spark"
 repos[fonctionel]="spec sql postman"
 
-repo_names=("devops" "data" "fonctionel")
-
-for repo_name in ${repo_names[@]}; do
-echo " repo name : "$repo_name
-for tool in ${repos[$repo_name]}; do
-echo "  tool name : "$tool
-./creation_script.sh $repo_name 2 $tool 300
-./reorganization_script.sh $repo_name
-done
+for repo_name in "${!repos[@]}"; do
+	echo " repo name : "$repo_name
+	for tool in ${repos[$repo_name]}; do
+		echo "  tool name : "$tool
+		./creation_script.sh "$repo_name" 2 "$tool" 300
+	done
+	./reorganization_script.sh "$repo_name"
 done
 
 echo "Script $0 : success."
 exit 0
+
+
+
+
